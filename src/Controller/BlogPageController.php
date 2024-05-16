484236754e1112e49bef767d3205500da5314235
Vitalij Procházka <vitek.prochazka1@seznam.cz>
2024-05-16 19:34:10 +0200

    Blog page hidden

--- src/Controller/BlogPageController.php
+++ src/Controller/BlogPageController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class BlogPageController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route('/blog', name: 'blog_page')]
     public function blogPage(): Response
     {
